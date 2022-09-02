e49be16791c025fd396ab7fe144edbe3f2218942
Vignesh Hedge <56476333+vignesh4a@users.noreply.github.com>
2022-09-02 23:26:54 +0530

    Update settings.py

--- recycle/recycle/settings.py
+++ recycle/recycle/settings.py
@@ -121,8 +121,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'recyclesdh@gmail.com'
-EMAIL_HOST_PASSWORD ='Hello@Team123'
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # Internationalization
